29414fc04c83529cc9f1868d6300881731ebeb05
Temmie <47639983+JustTemmie@users.noreply.github.com>
2021-11-21 14:10:09 +0100

    test

--- games/tetris/app.js
+++ games/tetris/app.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const width = 10
+
+    console.log(grid)
+    console.log(squares)
 
 
-
-
-})
+}) 
