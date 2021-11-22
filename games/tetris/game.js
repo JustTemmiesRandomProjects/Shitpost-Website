4e5937b4916b17623852654efde8529a6c98f1fd
Temmie <47639983+JustTemmie@users.noreply.github.com>
2021-11-22 12:08:53 +0100

    Update game.js

--- games/tetris/game.js
+++ games/tetris/game.js
@@ -21,10 +21,10 @@ document.addEventListener('DOMContentLoaded', () => {
   
     //The Tetrominoes
      const lTetromino = [
-        [1, width+1, width*2+1, 2],
+        [[1, width+1, width*2+1, 2],
         [width, width+1, width+2, width*2+2],
-        [1, width+1, width*2+1, width*2],
-        [width, width*2, width*2-1, width*2-2]
+        [1, width-1, width*2-1, width*2],
+        [width, width*2, width*2+1, width*2+2]
     ]
     const jTetromino = [
         [1, width+1, width*2+1, 2],
