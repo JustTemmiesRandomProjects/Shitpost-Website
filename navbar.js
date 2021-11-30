5218707a691c0e6066e74abecf8452f38d28dbf4
Temmie <47639983+JustTemmie@users.noreply.github.com>
2021-11-30 13:43:51 +0100

    update to look a loooot better

---
+++ navbar.js
@@ -0,0 +1,15 @@
+document.write('\
+\
+    <div class="navbar">\
+    <a href="https://temmietem.netlify.app/">Home</a>\
+    <a href="https://temmietem.netlify.app/games">Games</a>\
+    <a href="https://temmietem.netlify.app/rick">Rick</a>\
+    <a href="https://temmietem.netlify.app/shitposts">Shitposts</a>\
+    <a href="/" class="right">Back to main page</a>\
+    </div>\
+\
+     <p>Notice however, that you have to escape LF\'s with a \, just like\
+        demonstrated in this code listing.\
+    </p>\
+\
+')
