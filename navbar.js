0d60938eedbb2010252f0a7e563205ef35730c14
Temmie <47639983+JustTemmie@users.noreply.github.com>
2021-12-02 11:20:51 +0100

    jeffy

--- navbar.js
+++ navbar.js
@@ -5,6 +5,7 @@ document.write('\
     <a href="https://temmietem.netlify.app/games">Games</a>\
     <a href="https://temmietem.netlify.app/rick">Rick</a>\
     <a href="https://temmietem.netlify.app/shitposts">Shitposts</a>\
+    <a href="https://temmietem.netlify.app/websites_i_found_on_the_interwebs">Stuff I\'ve found</a>\
     <a href="/" class="right">Back to main page</a>\
     </div>\
 ')
