7c46fc8cca95156889d15957d373cb2f9ca91215
Temmie <47639983+JustTemmie@users.noreply.github.com>
2021-12-02 19:03:23 +0100

    beaver update!!

--- navbar.js
+++ navbar.js
@@ -2,6 +2,7 @@ document.write('\
 \
     <div class="navbar">\
     <a href="https://temmietem.netlify.app/">Home</a>\
+    <a href="https://temmietem.netlify.app/beaver">Beavers</a>\
     <a href="https://temmietem.netlify.app/games">Games</a>\
     <a href="https://temmietem.netlify.app/rick">Rick</a>\
     <a href="https://temmietem.netlify.app/shitposts">Shitposts</a>\
