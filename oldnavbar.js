b77cac9e438667d63f231208ee3db46989810fbc
Temmie <47639983+JustTemmie@users.noreply.github.com>
2021-12-15 19:59:36 +0100

    side nav bar

---
+++ oldnavbar.js
@@ -0,0 +1,12 @@
+document.write('\
+\
+    <div class="navbar">\
+    <a href="https://temmietem.site/">Home</a>\
+    <a href="https://temmietem.site/beaver">Beavers</a>\
+    <a href="https://temmietem.site/games">Games</a>\
+    <a href="https://temmietem.site/rick">Rick</a>\
+    <a href="https://temmietem.site/shitposts">Shitposts</a>\
+    <a href="https://temmietem.site/websites_i_found_on_the_interwebs">Stuff I\'ve found</a>\
+    <a href="/" class="right">Back to main page</a>\
+    </div>\
+')
---
+++ siteStyle.js
@@ -0,0 +1,70 @@
+document.write('\
+\
+<style>\
+* {\
+    box-sizing: border-box;\
+  }\
+  \
+  /* Style the body */\
+  body {\
+    font-family: Arial, Helvetica, sans-serif;\
+    margin: 0px;\
+  }\
+  \
+  /* Header/logo Title */\
+  .header {\
+    padding: 60px;\
+    text-align: center;\
+    background: #1abc9c;\
+    color: white;\
+  }\
+  \
+  /* Increase the font size of the heading */\
+  .header h1 {\
+    font-size: 40px;\
+  }\
+  \
+  \
+  /* Active/current link */\
+  .navbar a.active {\
+    background-color: #666;\
+    color: white;\
+  }\
+  \
+  /* Column container */\
+  .row {\
+    display: flex;\
+    flex-wrap: wrap;\
+  }\
+  \
+  /* Create two unequal columns that sits next to each other */\
+  \
+  /* left padding */\
+  .sidepadding {\
+    flex:8%;\
+    background-color: white;\
+    padding: 20px;\
+  }\
+  \
+  /* Sidebar/left column */\
+  .side {\
+    flex: 30%;\
+    background-color: #f1f1f1;\
+    padding: 20px;\
+  }\
+  \
+  /* Main column */\
+  .main {\
+    flex: 62%;\
+    background-color: white;\
+    padding: 20px;\
+  }\
+  \
+  /* Footer */\
+  .footer {\
+    padding: 20px;\
+    text-align: center;\
+    background: #ddd;\
+  }\
+  </style>\
+')
